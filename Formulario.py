1870a88246c9bbae4a61510a2875a713516cd2d7
emanuelsoamaral <emanuel.soares.amaral@gmail.com>
2025-08-07 22:01:35 -0300

    Update Formulario.py

--- Formulario.py
+++ Formulario.py
@@ -20,9 +20,9 @@ else:
 # Inputs do formulário
 col1, col2 = st.columns(2)
 
-data_emissao = st.text_input('Data:', key='data')
+col1 = st.text_input('Data:', key='data')
 
-valor = st.text_input('Valor', key='valor')
+col2 = st.text_input('Valor', key='valor')
 
 fornecedor = st.text_input('Fornecedor:', key='fornecedor')
 
@@ -35,9 +35,9 @@ padrao_data = r'^\d{2}/\d{2}/\d{4}$'      # dd/mm/aaaa
 padrao_valor = r'^\d{1,20},\d{2}$'         # até 6 dígitos antes da vírgula, 2 após
 
 if st.button('GRAVAR'):
-    if not re.match(padrao_data, data_emissao):
+    if not re.match(padrao_data, col1):
         st.error('Data inválida. Use o formato dd/mm/aaaa.')
-    elif not re.match(padrao_valor, valor):
+    elif not re.match(padrao_valor, col2):
         st.error('Valor inválido. Use o formato 9999,99.')
     elif not fornecedor or not descricao or not conta:
         st.error('Preencha todos os campos obrigatórios.')
@@ -46,8 +46,8 @@ if st.button('GRAVAR'):
             novo_id = int(df_vendas['id_venda'].max()) + 1 if not df_vendas.empty else 1
             nova_linha = pd.DataFrame([{
                 "id_venda": novo_id,
-                "data_emissao": data_emissao,
-                "valor": valor,
+                "data_emissao": col1,
+                "valor": col2,
                 "fornecedor": fornecedor,
                 "descricao": descricao,
                 "conta": conta  
